1f46a4011108dbe46181bd20b1704bf41e81db98
Liron Shalev <LironRS@users.noreply.github.com>
2021-04-11 22:17:53 +0300

    Remove comments

--- index.js
+++ index.js
@@ -17,10 +17,8 @@ const proxyMiddleware = createProxyMiddleware({
   onProxyReq: (proxyReq, req, res) => {
     if (req.body) {
         let bodyData = JSON.stringify(req.body);
-        // incase if content-type is application/x-www-form-urlencoded -> we need to change to application/json
         proxyReq.setHeader('Content-Type','application/json');
         proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
-        // stream the content
         proxyReq.write(bodyData);
     }
   },
